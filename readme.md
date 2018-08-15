8819f48f60f69d00bee9ce297b4c7c4db59c909e
BiosKill <bioski10@gmail.com>
2018-08-15 19:52:30 +0200

    commit

---
+++ readme.md
@@ -0,0 +1,7 @@
+## Projet site poutine
+
+#### Commandes utile:
+
+* ``npm update``
+* ``yarn encore dev``
+* ``php bin/console server:run``
---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index()
+    {
+        return $this->render('default/index.html.twig', [
+            'controller_name' => 'DefaultController',
+        ]);
+    }
+    /**
+     * @Route("/histoire", name="histoire")
+     */
+    public function histoire()
+    {
+        return $this->render('default/histoire.html.twig', [
+            'controller_name' => 'DefaultController',
+        ]);
+    }
+    /**
+     * @Route("/avis", name="avis")
+     */
+    public function avis()
+    {
+        return $this->render('default/avis.html.twig', [
+            'controller_name' => 'DefaultController',
+        ]);
+    }
+    /**
+     * @Route("/livraison", name="livraison")
+     */
+    public function livraison()
+    {
+        return $this->render('default/livraison.html.twig', [
+            'controller_name' => 'DefaultController',
+        ]);
+    }
+
+    /**
+     * @Route("/tabarnak", name="tabarnak")
+     */
+    public function tabarnak()
+    {
+        return $this->render('default/tabarnak.html.twig', [
+            'controller_name' => 'DefaultController',
+        ]);
+    }
+    /**
+     * @Route("/flibustiere", name="flibustiere")
+     */
+    public function flibustiere()
+    {
+        return $this->render('default/flibustiere.html.twig', [
+            'controller_name' => 'DefaultController',
+        ]);
+    }
+}
